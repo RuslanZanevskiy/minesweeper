285a348eef5ea5e6af1a83099e5a165f1d04c1b9
Ruslan Zan <reaasre@gmail.com>
2022-03-05 13:26:30 +0300

    basic graphics and logic(field generating)

--- gui.py
+++ gui.py
@@ -6,12 +6,13 @@ pg.init()
 
 class App:
     def __init__(self):
-        self.WIDTH, self.HEIGHT = (800, 600)
+        self.WIDTH, self.HEIGHT = (400, 600)
         self.screen = pg.display.set_mode((self.WIDTH, self.HEIGHT))
         self.FPS = 60
         self.clock = pg.time.Clock()
         self.TILE_SIZE = 40
         self.W_TILES, self.H_TILES = self.WIDTH // self.TILE_SIZE, self.HEIGHT // self.TILE_SIZE
+        print('app created')
 
     def check_input(self):
         pass
@@ -20,10 +21,37 @@ class App:
         pass
 
     def render_frame(self):
-        pass
+        self.screen.fill((200, 200, 200))
+
+        for i in range(self.WIDTH):
+            pg.draw.line(self.screen, (0, 0, 0), (i * self.TILE_SIZE, 0), (i * self.TILE_SIZE, self.HEIGHT))
+        for i in range(self.HEIGHT):
+            pg.draw.line(self.screen, (0, 0, 0), (0, i * self.TILE_SIZE), (self.WIDTH, i * self.TILE_SIZE))
+
+        hts = self.TILE_SIZE // 2
+        ts10 = self.TILE_SIZE // 10
+        font = pg.font.SysFont('arial', round(self.TILE_SIZE / 1.5))
+        for y, row in enumerate(self.field.data):
+            for x, col in enumerate(row):
+                if col == -1:
+                    pg.draw.circle(self.screen, (0, 0, 0), (x * self.TILE_SIZE + hts, y * self.TILE_SIZE + hts), hts // 1.5)
+                elif col != 0:
+                    text = font.render(str(col), True, (0, 0, 0))
+                    self.screen.blit(text, (x * self.TILE_SIZE + hts - ts10 , y * self.TILE_SIZE + ts10))
+        pg.display.update()
 
     def run(self):
+        print('running...')
         is_running = True
+
+
+        self.field = ms_core.Field(self.W_TILES, self.H_TILES, round(self.W_TILES * self.H_TILES * 0.15))
+        for row in self.field.data:
+            for col in row:
+                sym = ' ' if col == 0 else '*' if col == -1 else str(col)
+                print(sym, end='')
+            print()
+        
 
         while is_running:
             self.clock.tick(self.FPS)
--- minesweeper.py
+++ minesweeper.py
@@ -2,7 +2,72 @@ import random
 
 
 class Field:
-    pass
+    def __init__(self, w, h, number):
+        self.W = w
+        self.H = h
+        self.data = [[0] * w for _ in range(h)]
+        self.N = number
+        self.generate()
+        print('field created')
+
+    def get_neighbors(self, x, y):
+        w, h = self.W, self.H
+        nei = 0
+        if x != 0 and self.data[y][x-1] == -1:
+            nei += 1
+        if x != w-1 and self.data[y][x+1] == -1:
+            nei += 1
+        if y != 0 and self.data[y-1][x] == -1:
+            nei += 1
+        if y != h-1 and self.data[y+1][x] == -1:
+            nei += 1
+        
+        if x != 0 and y != 0 and self.data[y-1][x-1] == -1:
+            nei += 1
+        if x != w-1 and y != 0 and self.data[y-1][x+1] == -1:
+            nei += 1
+        if x != 0 and y != h-1 and self.data[y+1][x-1] == -1:
+            nei += 1
+        if x != w-1 and y != h-1 and self.data[y+1][x+1] == -1:
+            nei += 1
+
+        return nei
+
+    def generate(self):
+        print('start generating field')
+        k = 0
+        while k < self.N:
+            x, y = random.randint(0, self.W-1), random.randint(0, self.H-1) 
+            if self.data[y][x] != -1:
+                k += 1
+                self.data[y][x] = -1
+        print('mines generated')
+
+        for y in range(self.H):
+            for x in range(self.W):
+                if self.data[y][x] != -1:
+                    nei = self.get_neighbors(x, y)
+                    self.data[y][x] = nei    
+        print('number blocks generated')
+        
 
 class Game:
-    pass
+    def __init__(self, field):
+        self.field = field
+        self.clicked = []
+        self.flaged = []
+
+    def dfs(self, x, y, vis):
+        if [x, y] in self.flaged:
+            return
+        if self.field.data[y][x] == -1:
+            pass
+        
+
+    def get_vision(self):
+        vis = [[False] * self.field.W for _ in range(self.field.H)]
+
+        for x, y in self.clicked:
+            self.dfs(x, y, vis)
+
+    
